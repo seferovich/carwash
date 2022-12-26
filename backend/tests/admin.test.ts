488798d6ad5a2baef9d7009435612c7664799a5f
sefkelol <seferovicisa@gmail.com>
2022-12-26 23:16:38 +0100

    first commit

---
+++ backend/tests/admin.test.ts
@@ -0,0 +1,91 @@
+import app from "../src/app"
+import request from "supertest"
+import mongoose from "mongoose"
+import Admin from "../src/models/adminModel"
+import jwt from "jsonwebtoken"
+
+
+const adminOneId = new mongoose.Types.ObjectId
+
+const adminOne = {
+    _id: adminOneId,
+    username: 'TestAdminOne',
+    password: '123456ekl',
+    tokens:[{
+        token: jwt.sign({_id: adminOneId}, process.env.JWT_SECRET as string)
+    }]
+}
+
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_TEST_URL as string)
+})
+
+afterAll(async () => {
+    // await Admin.deleteMany()
+    await mongoose.disconnect()
+})
+
+describe('POST, DELETE, GET /api/admin', () => {
+
+    describe('Given an username and password', () => {
+
+        test('Should create a new admin account', async () => {
+            const response = await request(app)
+            .post('/api/admin/create')
+            .send({
+                username: 'test',
+                password: '123456ekl'
+            })
+            .expect(201)
+
+            // Assert that the admin is in the DB
+            const admin = await Admin.findById(response.body.admin._id)
+            expect(admin).not.toBeNull()
+        })
+
+        test('Should login into an existing admin account', async () => {
+            const response = await request(app)
+            .post('/api/admin/login')
+            .send({
+                username: 'test',
+                password: '123456ekl'
+            })
+            .expect(200)
+        })  
+
+    })
+
+    describe('Given a jwt token', () => {
+        // Make a new admin acc before each test, because the token gets deleted after log out.
+        beforeEach(async () => {
+            await Admin.deleteMany()
+            await new Admin(adminOne).save()
+        })
+
+        test('Should get admin account info', async () => {
+            const response = await request(app)
+            .get('/api/admin/get')
+            .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+        })
+
+        test('Should log out of the admin account', async () => {
+            const response = await request(app)
+            .post('/api/admin/logout')
+            .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+        })
+
+        test('Should delete an admin account', async () => {           
+            const response = await request(app)
+            .delete('/api/admin/delete')
+            .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+        })
+
+    })
+})
---
+++ backend/tests/order.test.ts
@@ -0,0 +1,185 @@
+import app from "../src/app"
+import request, { Response } from "supertest"
+import mongoose from "mongoose"
+import Admin from "../src/models/adminModel"
+import Customer from "../src/models/customerModel"
+import jwt from "jsonwebtoken"
+import Order from "../src/models/orderModel"
+import { IOrder } from "../src/global/interfaces"
+
+
+
+const adminOneId = new mongoose.Types.ObjectId
+const adminOne = {
+    _id: adminOneId,
+    username: 'TestAdminOne',
+    password: '123456ekl',
+    tokens:[{
+        token: jwt.sign({_id: adminOneId}, process.env.JWT_SECRET as string)
+    }]
+}
+const customerOneId = new mongoose.Types.ObjectId
+const customerOne = {
+    _id: customerOneId,
+    name: 'Test Customer',
+    dob: '1969-04-20'
+}
+
+const customerTwoId = new mongoose.Types.ObjectId
+const customerTwo = {
+    _id: customerTwoId,
+    name: 'Test Customer2',
+    dob: '1969-04-20',
+    points: 10
+}
+
+const orderOneId = new mongoose.Types.ObjectId
+const orderOne = {
+    orders:[
+    {
+        name: "Tyre wash",
+        price: 8,
+        selected: true
+    },
+    {
+        name: "Body wash",
+        price: 12,
+        selected: false
+    },
+    {
+        name: "Interior washing",
+        price: 12,
+        selected: false
+    },
+    {
+        name: "Interior vacumming",
+        price: 8,
+        selected: true
+    }
+],
+_id: orderOneId,
+customer: customerOneId
+}
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_TEST_URL as string)
+    await new Admin(adminOne).save()
+})
+
+afterAll(async () => {
+    await Admin.deleteMany()
+    await Customer.deleteMany()
+    await Order.deleteMany()
+    await mongoose.disconnect()
+})
+
+
+
+describe('POST, DELETE, GET /api/orders', () => {
+    describe('When admin is authenticated', () => {
+
+        test('Should create a new order, with and without a customer _id provided', async () => {
+            const responseOne = await request(app)
+            .post('/api/orders/create')
+            .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+            .send({
+                orders:[
+                {
+                    name: "Tyre wash",
+                    price: 8,
+                    selected: true
+                },
+                {
+                    name: "Body wash",
+                    price: 12,
+                    selected: false
+                },
+                {
+                    name: "Interior washing",
+                    price: 12,
+                    selected: false
+                },
+                {
+                    name: "Interior vacumming",
+                    price: 8,
+                    selected: true
+                }
+            ],
+            customer: customerOneId
+            })
+            .expect(201)
+
+            expect(responseOne.body.total).toEqual(16)
+
+
+            // No customer _id
+            const responseTwo = await request(app)
+            .post('/api/orders/create')
+            .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+            .send({
+                orders:[
+                {
+                    name: "Tyre wash",
+                    price: 8,
+                    selected: true
+                },
+                {
+                    name: "Body wash",
+                    price: 12,
+                    selected: false
+                },
+                {
+                    name: "Interior washing",
+                    price: 12,
+                    selected: false
+                },
+                {
+                    name: "Interior vacumming",
+                    price: 8,
+                    selected: true
+                }
+            ]})
+            .expect(201)
+        })
+        describe('Given different customers', () => {
+            test('Should give a 20% discount if customer has 10 points', async () => {
+                const customer = await new Customer(customerTwo).save()
+
+                const response = await request(app)
+                .post('/api/orders/create')
+                .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+                .send({
+                    orders:[
+                    {
+                        name: "Tyre wash",
+                        price: 8,
+                        selected: true
+                    },
+                    {
+                        name: "Body wash",
+                        price: 12,
+                        selected: false
+                    },
+                    {
+                        name: "Interior washing",
+                        price: 12,
+                        selected: false
+                    },
+                    {
+                        name: "Interior vacumming",
+                        price: 8,
+                        selected: true
+                    },
+                    
+                ],
+                    customer: customerTwoId
+                }).expect(201)
+
+
+                // FIXME: Response gives back 16
+                expect(response.body.total).toEqual(12.8)
+            
+            })
+        })
+    })
+})
