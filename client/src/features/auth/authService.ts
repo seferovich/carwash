dd68c94aa2af93aaaf4ad3d78105f34c5e428e1c
sefkelol <seferovicisa@gmail.com>
2023-01-09 14:38:11 +0100

    Heroku test

--- client/src/features/auth/authService.ts
+++ client/src/features/auth/authService.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 import {IAdmin} from "../../globals/interfaces"
 
-
+const link = process.env.PORT || 'http://localhost:5001'
 const login = async (adminData: IAdmin) => {
     // Added this link so it works on Heroku
-    const response = await axios.post('https://carwash.herokuapp.com/api/admin/login', adminData)
+    const response = await axios.post('/api/admin/login', adminData)
 
     if (response.data){
         localStorage.setItem('jwt', JSON.stringify(response.data.token))
